6c720c880a0b66220ea77e0084ef05d42919b20e
rodanAsh <aakashash2002@gmail.com>
2024-09-10 16:01:03 +0530

    initial commit

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,56 @@
+import React, { useState } from 'react'
+import menu from '../../public/assets/menu.svg'
+import close from '../../public/assets/close.svg'
+import { navLinks } from '../constants/index.js'
+
+
+const NavItems = () => {
+  return (
+    <ul className='nav-ul'>
+      {
+        navLinks.map(({id,name,href}) => (
+        <li key={id} className='nav-li'>
+          <a href={href} className='nav-li_a'>{name}</a>
+        </li>
+      ))}
+    </ul>
+  )   
+}
+
+const Navbar = () => {
+  const [isOpen,setIsOpen] = useState(false)
+
+  const toggleMenu= () => {
+    setIsOpen(prev => !prev)
+  }
+  return (
+    <header className='fixed top-0 left-0 right-0 z-50 bg-black/90'>
+        <div className='max-w-7xl mx-auto'>
+            <div className='flex justify-between items-center py-5 mx-auto c-space'>
+                <a href="/" className='text-neutral-400 font-bold text-xl hover:text-white transition-colors'>
+                    Aakash
+                </a>
+
+                <button onClick={toggleMenu} className='text-neutral-400 hover:text-white focus:outline-none sm:hidden flex'>
+                  <img src={isOpen ? close : menu} alt="menu" className='w-6 h-6' />
+                </button>
+
+                <nav className='sm:flex hidden'>
+                  <NavItems />
+                </nav>
+            </div>
+        </div>
+        <div className='nav-sidebar'>
+          {
+            isOpen && (
+              <nav className='py-5'>
+                <NavItems />
+              </nav>
+            )
+          }
+        </div>   
+    </header>
+  )
+}
+
+export default Navbar
