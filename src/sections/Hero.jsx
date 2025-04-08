d245a082f1e611c952edc2356a8c04f477381d9c
rodanAsh <aakashash2002@gmail.com>
2025-04-08 13:09:18 +0530

    chore : added my updated resume

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -100,7 +100,7 @@ const Hero = () => {
             </h1>
             <button className="ml-3">
               <a
-                href="https://drive.google.com/file/d/1zRrio6UyoEe6gqK6j1lN0alINff-ueLM/view"
+                href="https://drive.google.com/file/d/1QWUIBdpsxKoLdOBk_8BI_rF4cGpR_bGe/view"
                 target="_blank"
                 className="group relative flex w-fit items-center justify-center overflow-hidden rounded-md border-2 border-slate-900 bg-slate-50 px-4 py-2 font-bold transition-transform ease-out hover:scale-105"
               >
